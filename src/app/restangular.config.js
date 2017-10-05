4e2721c3b14e30c5ce8e0f6824d3d7435240212a
Zed_Desktop <platae93@gmail.com>
2017-10-04 19:16:09 -0500

    Manejo dinamico de apis para restangular

--- src/app/restangular.config.js
+++ src/app/restangular.config.js
@@ -6,9 +6,25 @@
         .module('app')
         .config(config);
 
-    function config(RestangularProvider, EnvironmentConfig,$httpProvider) {
+    function config(RestangularProvider, EnvironmentConfig, $httpProvider) {
+        var baseUrl = null;
+        switch (EnvironmentConfig.environment) {
+            case 'development':
+                //EnvironmentConfig.site.rest.api+'mobile-dev/oauth/'
+                baseUrl = EnvironmentConfig.site.rest.api;
+                break;
+            case 'staging':
+                baseUrl = EnvironmentConfig.site.rest.api;
+                break;
+            case 'production':
+                baseUrl = EnvironmentConfig.site.rest.api;
+                break;
+            case 'local':
+                baseUrl = EnvironmentConfig.site.rest.api;
+                break;
+        }
 
-        RestangularProvider.setBaseUrl(EnvironmentConfig.site.rest.api);
+        RestangularProvider.setBaseUrl(baseUrl);
         $httpProvider.interceptors.push('AuthInterceptor');
         //RestangularProvider.setDefaultHeaders({'Content-Type': "Application/JSON"});
         //RestangularProvider.setExtraFields(['name']);
