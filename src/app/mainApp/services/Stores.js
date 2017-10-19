99619082017b560376062f6550552630617fa7fa
PlataE93 <platae93@gmail.com>
2017-10-18 21:08:40 -0500

    Se creó el service para el catálogo de establecimientos

---
+++ src/app/mainApp/services/Stores.js
@@ -0,0 +1,57 @@
+(function () {
+    angular
+        .module('app.mainApp')
+        .factory('Stores', Stores);
+
+    function Stores(Restangular, EnvironmentConfig, URLS) {
+        var baseURL = null;
+        switch (EnvironmentConfig.environment) {
+            case 'development':
+                baseURL = Restangular.all(URLS.environment.mobile_dev).all('establecimiento');
+                break;
+            case 'staging':
+                baseURL = Restangular.all(URLS.environment.mobile_stg).all('establecimiento');
+                break;
+            case 'production':
+                baseURL = Restangular.all(URLS.environment.mobile).all('establecimiento');
+                break;
+            case 'local':
+                baseURL = Restangular.all(URLS.environment.mobile_local).all('establecimiento');
+                break;
+        }
+    }
+
+    return {
+        list: list,
+        getByID: getByID,
+        create: create,
+        remove: remove,
+        update: update
+    }
+
+    function list() {
+        return baseURL.getList();
+    }
+
+    function getByID(id) {
+        return baseURL.all(id).customGET();
+    }
+
+    function create(object) {
+        return baseURL.customPOST(object);
+    }
+
+    function remove(id) {
+        return baseURL.customDELETE(id, null, {'content-type': 'application/json'});
+    }
+
+    function update(object, id) {
+        if (id) {
+            return baseURL.all(id).customPUT(object);
+        }
+        else {
+            return baseURL.all(object.id).customPUT(object);
+        }
+    }
+
+})();
