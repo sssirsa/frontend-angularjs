71472d3694d0a286af246afddf22cb840f301fe0
PlataE93 <platae93@gmail.com>
2017-10-18 22:56:27 -0500

    Se crearon las entradas en el menú de administrados para mostrar los catálogos

--- src/app/mainApp/catalogos/routes/routes.controller.js
+++ src/app/mainApp/catalogos/routes/routes.controller.js
@@ -3,11 +3,11 @@
 
     angular
         .module('app.mainApp.catalogos')
-        .controller('LineaTransporteController', LineaTransporteController)
+        .controller('routesController', routesController)
         .filter('lineaSearch', custom);
 
     /* @ngInject */
-    function LineaTransporteController(LineaTransporte,Helper, $scope, toastr, Translate,$mdDialog) {
+    function routesController(Routes, Helper, $scope, toastr, Translate, $mdDialog) {
 
         var vm = this;
 
@@ -19,8 +19,8 @@
         vm.cancel = cancel;
         vm.create = create;
         vm.restore = restore;
-        vm.remove=remove;
-        vm.update=update;
+        vm.remove = remove;
+        vm.update = update;
         vm.search_items = [];
         vm.searchText = '';
         var transport = {
@@ -31,12 +31,13 @@
         };
         vm.transport = angular.copy(transport);
         vm.numberBuffer = '';
-        vm.myHeight=window.innerHeight-250;
-        vm.myStyle={"min-height":""+vm.myHeight+"px"};
+        vm.myHeight = window.innerHeight - 250;
+        vm.myStyle = {"min-height": "" + vm.myHeight + "px"};
         vm.toggleDeleted = true;
 
         activate();
         init();
+
         function init() {
             vm.successTitle = Translate.translate('MAIN.MSG.SUCCESS_TITLE');
             vm.errorTitle = Translate.translate('MAIN.MSG.ERROR_TITLE');
@@ -44,14 +45,14 @@
             vm.errorMessage = Translate.translate('MAIN.MSG.ERROR_MESSAGE');
             vm.successUpdateMessage = Translate.translate('MAIN.MSG.GENERIC_SUCCESS_UPDATE');
             vm.successDeleteMessage = Translate.translate('MAIN.MSG.GENERIC_SUCCESS_DELETE');
-            vm.deleteButton=Translate.translate('MAIN.BUTTONS.DELETE');
-            vm.cancelButton=Translate.translate('MAIN.BUTTONS.CANCEL');
-            vm.dialogTitle=Translate.translate('MAIN.DIALOG.DELETE_TITLE');
-            vm.dialogMessage=Translate.translate('MAIN.DIALOG.DELETE_MESSAGE');
-            vm.duplicateMessage=Translate.translate('TRANSPORT_LINE.FORM.LABEL.DUPLICATE');
-            vm.dialogRestoreTitle=Translate.translate('MAIN.DIALOG.RESTORE_TITLE');
-            vm.dialogRestoreMessage=Translate.translate('MAIN.DIALOG.RESTORE_MESSAGE');
-            vm.restoreButton=Translate.translate('MAIN.BUTTONS.RESTORE');
+            vm.deleteButton = Translate.translate('MAIN.BUTTONS.DELETE');
+            vm.cancelButton = Translate.translate('MAIN.BUTTONS.CANCEL');
+            vm.dialogTitle = Translate.translate('MAIN.DIALOG.DELETE_TITLE');
+            vm.dialogMessage = Translate.translate('MAIN.DIALOG.DELETE_MESSAGE');
+            vm.duplicateMessage = Translate.translate('TRANSPORT_LINE.FORM.LABEL.DUPLICATE');
+            vm.dialogRestoreTitle = Translate.translate('MAIN.DIALOG.RESTORE_TITLE');
+            vm.dialogRestoreMessage = Translate.translate('MAIN.DIALOG.RESTORE_MESSAGE');
+            vm.restoreButton = Translate.translate('MAIN.BUTTONS.RESTORE');
             vm.successRestoreMessage = Translate.translate('MAIN.MSG.GENERIC_SUCCESS_RESTORE');
         }
 
@@ -59,10 +60,12 @@
         function activate() {
             listlineas();
         }
+
         function toggleDeletedFunction() {
             listlineas();
             cancel();
         }
+
         function remove(ev) {
             var confirm = $mdDialog.confirm()
                 .title(vm.dialogTitle)
@@ -70,48 +73,48 @@
                 .ariaLabel('Confirmar eliminación')
                 .ok(vm.deleteButton)
                 .cancel(vm.cancelButton);
-            $mdDialog.show(confirm).then(function() {
-                LineaTransporte.remove(vm.transport).then(function (res) {
+            $mdDialog.show(confirm).then(function () {
+                Routes.remove(vm.transport).then(function (res) {
                     toastr.success(vm.successDeleteMessage, vm.successTitle);
                     cancel();
                     activate();
                 }).catch(function (res) {
                     toastr.warning(vm.errorMessage, vm.errorTitle);
                 });
-            }, function() {
+            }, function () {
 
             });
         }
 
         function update() {
-            LineaTransporte.update(vm.transport).then(function (res) {
+            Routes.update(vm.transport).then(function (res) {
                 toastr.success(vm.successUpdateMessage, vm.successTitle);
                 cancel();
                 activate();
             }).catch(function (err) {
-                if(err.status==400 && err.data.razon_social!=undefined)
-                {
-                    toastr.error(vm.duplicateMessage,vm.errorTitle);
-                }else {
+                if (err.status == 400 && err.data.razon_social != undefined) {
+                    toastr.error(vm.duplicateMessage, vm.errorTitle);
+                } else {
                     toastr.error(vm.errorMessage, vm.errorTitle);
                 }
             });
         }
+
         function create() {
-            LineaTransporte.create(vm.transport).then(function (res) {
+            Routes.create(vm.transport).then(function (res) {
                 toastr.success(vm.successCreateMessage, vm.successTitle);
                 vm.transport = angular.copy(transport);
                 cancel();
                 activate();
             }).catch(function (err) {
-                if(err.status==400 && err.data.razon_social!=undefined)
-                {
-                    toastr.error(vm.duplicateMessage,vm.errorTitle);
-                }else{
-                    toastr.error(vm.errorMessage,vm.errorTitle);
+                if (err.status == 400 && err.data.razon_social != undefined) {
+                    toastr.error(vm.duplicateMessage, vm.errorTitle);
+                } else {
+                    toastr.error(vm.errorMessage, vm.errorTitle);
                 }
             });
         }
+
         function restore() {
             var confirm = $mdDialog.confirm()
                 .title(vm.dialogRestoreTitle)
@@ -119,47 +122,49 @@
                 .ariaLabel('Confirmar restauración')
                 .ok(vm.restoreButton)
                 .cancel(vm.cancelButton);
-            $mdDialog.show(confirm).then(function() {
-                vm.transport.deleted=false;
-                LineaTransporte.update(vm.transport).then(function (res) {
+            $mdDialog.show(confirm).then(function () {
+                vm.transport.deleted = false;
+                Routes.update(vm.transport).then(function (res) {
                     toastr.success(vm.successRestoreMessage, vm.successTitle);
                     cancel();
                     activate();
                 }).catch(function (res) {
-                    vm.transport.deleted=true;
+                    vm.transport.deleted = true;
                     toastr.warning(vm.errorMessage, vm.errorTitle);
                 });
-            }, function() {
+            }, function () {
 
             });
 
         }
+
         function cancel() {
             $scope.TransportForm.$setPristine();
             $scope.TransportForm.$setUntouched();
             vm.transport = angular.copy(transport);
             vm.selectedLineaList = null;
-            vm.numberBuffer=null;
-            vm.searchText=null;
+            vm.numberBuffer = null;
+            vm.searchText = null;
         }
-        function listlineas()
-        {
-            vm.loadingPromise = LineaTransporte.listObject().then(function (res) {
-                vm.lineas =Helper.filterDeleted(res,vm.toggleDeleted);
-                vm.lineas=_.sortBy(vm.lineas, 'razon_social');
-            }).catch(function(err){
+
+        function listlineas() {
+            vm.loadingPromise = Routes.listObject().then(function (res) {
+                vm.lineas = Helper.filterDeleted(res, vm.toggleDeleted);
+                vm.lineas = _.sortBy(vm.lineas, 'razon_social');
+            }).catch(function (err) {
 
             });
         }
-        function selectedItemChange(item)
-        {
-            if (item!=null) {
+
+        function selectedItemChange(item) {
+            if (item != null) {
                 vm.transport = angular.copy(item);
 
-            }else{
+            } else {
                 cancel();
             }
         }
+
         function selectedLineas(project) {
             vm.selectedLineaList = project;
             vm.transport = angular.copy(project);
