0e81ea0a851d4535336f2c3a06750615f5650bad
Zed_Desktop <platae93@gmail.com>
2018-01-18 11:53:50 -0600

    Se crearon los factories para tener restangulars separados

---
+++ src/app/services/WebRestangular.js
@@ -0,0 +1,12 @@
+(function(){
+    angular
+        .module('app')
+        .factory('WebRestangular', WebRestangular);
+
+    function WebRestangular(Restangular, EnvironmentConfig) {
+        return Restangular.withConfig(function(RestangularConfigurer){
+            RestangularConfigurer.setBaseUrl(EnvironmentConfig.site.rest['web-api']);
+        });
+    }
+
+})();
