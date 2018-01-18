4f90d57af13eba8fd999ce5df0d0408c24e5cc2d
Zed_Desktop <platae93@gmail.com>
2018-01-18 17:13:17 -0600

    Se añadieron returns faltantes en el provider de servicios

--- src/app/mainApp/services/servicios.service.js
+++ src/app/mainApp/services/servicios.service.js
@@ -41,12 +41,12 @@
         var baseModelo = WebRestangular.all(URLS.etapa_servicio);
 
         function crearEtapaServicio(etapa) {
-            baseModelo.customPOST(etapa);
+            return baseModelo.customPOST(etapa);
         }
 
 
         function editarEtapaServicio(etapa) {
-            baseModelo.all(etapa.id).customPUT(etapa);
+            return baseModelo.all(etapa.id).customPUT(etapa);
         }
 
 
@@ -55,18 +55,18 @@
         }
 
         function consultarAllEtapaServicioDiagnostico(etapa) {
-            baseModelo.one('diagnostic', etapa.id).customGET();
+            return baseModelo.one('diagnostic', etapa.id).customGET();
         }
 
         function consultarEtapaServicioDiagnostico(diagnostico) {
-            baseModelo.all('diagnostic').one('latest', diagnostico.id).customGET();
+            return baseModelo.all('diagnostic').one('latest', diagnostico.id).customGET();
         }
         function consultarSalidaServicioDiagnostico(diagnostico) {
-            baseModelo.all('diagnostic').one('latest', diagnostico.id).customGET();
+            return baseModelo.all('diagnostic').one('latest', diagnostico.id).customGET();
         }
 
         function consultarAllInsumosCabinetEtapa(etapa) {
-            baseModelo.one('insumos', etapa.id).customGET();
+            return baseModelo.one('insumos', etapa.id).customGET();
         }
 
 
@@ -87,61 +87,61 @@
         }
 
         function getDiagnosticoFromCabinet(idCabinet) {
-            WebRestangular.all(URLS.diagnostico).all('latest').one(idCabinet).customGET();
+            return WebRestangular.all(URLS.diagnostico).all('latest').one(idCabinet).customGET();
         }
 
         function anadirInsumo(insumo) {
-            WebRestangular.all(URLS.insumo_usado).customPOST(insumo);
+            return WebRestangular.all(URLS.insumo_usado).customPOST(insumo);
         }
 
         function modificarInsumo(insumo) {
-            WebRestangular.all(URLS.insumo_usado).one('', insumo.id).customPUT(insumo);
+            return WebRestangular.all(URLS.insumo_usado).one('', insumo.id).customPUT(insumo);
         }
 
         function eliminarInsumo(insumo) {
-            WebRestangular.all(URLS.insumo_usado).one('', insumo.id).customDELETE(insumo.id, null, {'content-type': 'application/json'});
+            return WebRestangular.all(URLS.insumo_usado).one('', insumo.id).customDELETE(insumo.id, null, {'content-type': 'application/json'});
         }
 
         function consultarInfoCabinet(idcabinet) {
-            WebRestangular.one(URLS.cabinet, idcabinet).customGET();
+            return WebRestangular.one(URLS.cabinet, idcabinet).customGET();
         }
 
         function consultarInsumobyNombre(cadena) {
-            WebRestangular.all(URLS.catalogo_insumos).one('lookup', cadena).customGET();
+            return WebRestangular.all(URLS.catalogo_insumos).one('lookup', cadena).customGET();
 
         }
 
         function getCatalogoInsumoById(catalogo) {
-            WebRestangular.one(URLS.catalogo_insumos, catalogo.id).customGET();
+            return WebRestangular.one(URLS.catalogo_insumos, catalogo.id).customGET();
 
         }
 
         function consultarInsumosEtapa(etapa) {
-            WebRestangular.one('Insumos', etapa.actual_etapa).customGET(etapa);
+            return WebRestangular.one('Insumos', etapa.actual_etapa).customGET(etapa);
         }
 
         //Nuevos Endpoints por nuevos requerimentos y reeconstruccion Etapa Servicio
         function BusquedaCatalogoTypeStep(data) {
-            WebRestangular.all(URLS.catalogo_insumos).one('tipo', data.idTipo).all('etapa').customGET(data.idEtapa);
+            return WebRestangular.all(URLS.catalogo_insumos).one('tipo', data.idTipo).all('etapa').customGET(data.idEtapa);
         }
 
         function BusquedaInsumosTypeStep(data) {
-            WebRestangular.all(URLS.insumo).one('tipo', data.idTipo).one('etapa', data.idEtapa).customGET();
+            return WebRestangular.all(URLS.insumo).one('tipo', data.idTipo).one('etapa', data.idEtapa).customGET();
 
         }
 
         function etapaList() {
-            WebRestangular.all(URLS.etapa).customGET();
+            return WebRestangular.all(URLS.etapa).customGET();
         }
 
         function cabinetByEconomic(economico) {
-            WebRestangular.all('model').one('cabinet', economico).customGET();
+            return WebRestangular.all('model').one('cabinet', economico).customGET();
 
         }
 
         // /etapa_servicio/first/
         function firstStepByDiagnostic(diagnostico) {
-            baseModelo.one('first', diagnostico.id).customGET();
+            return baseModelo.one('first', diagnostico.id).customGET();
         }
 
 
