aaf74e9c20e44584286c75075d0a5447e03523c8
Zed_Desktop <platae93@gmail.com>
2018-01-18 10:53:27 -0600

    Se creó el provider de solicitudesPV, se crearon archivo plantilla para los módulos de listado y creación de solicitudes

---
+++ src/app/mainApp/servicios/solicitud/list/list-requests-page.controller.js
@@ -0,0 +1,51 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('requests')
+        .controller('ListRequestPageController', ListRequestPageController);
+
+    /* @ngInject */
+    function ListRequestPageController($state) {
+        var vm = this;
+
+        //Function mapping
+        vm.listRequests = listRequests;
+        vm.selectRequest = selectRequest;
+
+        //Variable declaration
+        vm.requests = null;
+
+        activate();
+
+        function activate(){
+
+        }
+
+        function listRequests(requestKind) {
+            switch (requestKind) {
+                case 'Abierta':
+                    vm.requests = vm.pendingRequests;
+                    break;
+                case 'Atendida':
+                    vm.requests = vm.attendedRequests;
+                    break;
+                case 'Cancelada':
+                    vm.requests = vm.cancelledRequests;
+                    break;
+                case 'Cerrada':
+                    vm.requests = vm.closedRequests;
+                    break;
+                case 'En_proceso':
+                    vm.requests = vm.inProcessRequests;
+                    break;
+            }
+
+        }
+
+        function selectRequest(request) {
+            $state.go('triangular.detailRequest', {id: request.folio});
+        }
+
+    }
+})();
