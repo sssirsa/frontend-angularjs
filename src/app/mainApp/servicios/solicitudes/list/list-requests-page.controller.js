0f358e2bebd3be060f5f388727f2048650ff68e5
PlataE93 <platae93@gmail.com>
2018-01-20 16:10:07 -0600

    Se añadieron los translates para la pantalla de listado.
    Se adecuó la función de búsqueda de solicitudes por tipo

--- src/app/mainApp/servicios/solicitudes/list/list-requests-page.controller.js
+++ src/app/mainApp/servicios/solicitudes/list/list-requests-page.controller.js
@@ -6,7 +6,7 @@
         .controller('ListRequestPageController', ListRequestPageController);
 
     /* @ngInject */
-    function ListRequestPageController($state) {
+    function ListRequestPageController($state, $log, toastr, SalePointRequests, Translate) {
         var vm = this;
 
         //Function mapping
@@ -15,142 +15,24 @@
 
         //Variable declaration
         vm.selectedKind = null;
-        vm.requestKinds = [
-            {id: 'pending', value: 'Abiertas'},
-            {id: 'attended', value: 'Atendidas'}
-        ];
+        vm.allRequests = null;
 
-        vm.requests = null;
-        vm.attendedRequests = [
-            {
-                folio: 'CUN-01-PROD-17',
-                estado: 'Atendida',
-                fecha: '27/11/2017 13:00:00'
-            },
-            {
-                folio: 'CUN-02-PROD-17',
-                estado: 'Atendida',
-                fecha: '27/11/2017 13:09:54'
-            },
-            {
-                folio: 'MEX-01-HANG-17',
-                estado: 'Atendida',
-                fecha: '26/10/2017 07:12:33'
-            },
-            {
-                folio: 'HUX-01-BAND-17',
-                estado: 'Atendida',
-                fecha: '27/10/2017 07:54:02'
-            }
-        ];
-        vm.pendingRequests = [
-            {
-                folio: 'MTY-01-PROD-17',
-                estado: 'Abierta',
-                fecha: '24/11/2017 13:45:57'
-            },
-            {
-                folio: 'CUN-03-PROD-17',
-                estado: 'Abierta',
-                fecha: '20/11/2017 13:09:54'
-            },
-            {
-                folio: 'MEX-02-HANG-17',
-                estado: 'Abierta',
-                fecha: '27/10/2017 07:12:33'
-            },
-            {
-                folio: 'HUX-02-BAND-17',
-                estado: 'Abierta',
-                fecha: '22/10/2017 07:54:02'
-            },
-            {
-                folio: 'MTY-02-HANG-17',
-                estado: 'Abierta',
-                fecha: '25/11/2017 13:45:57'
-            },
-            {
-                folio: 'CUN-04-BAND-17',
-                estado: 'Abierta',
-                fecha: '21/11/2017 13:09:54'
-            },
-            {
-                folio: 'MEX-03-CORP-17',
-                estado: 'Abierta',
-                fecha: '27/10/2017 11:12:33'
-            },
-            {
-                folio: 'HUX-03-HANG-17',
-                estado: 'Abierta',
-                fecha: '23/10/2017 07:54:02'
-            }
-        ];
-        vm.cancelledRequests = [
-            {
-                folio: 'CUN-05-PROD-17',
-                estado: 'Cancelada',
-                fecha: '27/11/2017 13:00:00'
-            },
-            {
-                folio: 'CUN-06-PROD-17',
-                estado: 'Cancelada',
-                fecha: '27/11/2017 13:09:54'
-            },
-            {
-                folio: 'MEX-04-HANG-17',
-                estado: 'Cancelada',
-                fecha: '26/10/2017 07:12:33'
-            },
-            {
-                folio: 'HUX-04-BAND-17',
-                estado: 'Cancelada',
-                fecha: '27/10/2017 07:54:02'
-            }
-        ];
-        vm.closedRequests = [
-            {
-                folio: 'CUN-08-PROD-17',
-                estado: 'Cerrada',
-                fecha: '27/11/2017 13:00:00'
-            },
-            {
-                folio: 'HUX-05-BAND-17',
-                estado: 'Cerrada',
-                fecha: '27/10/2017 07:54:02'
-            }
-        ];
-        vm.inProcessRequests = [
-            {
-                folio: 'CUN-07-PROD-17',
-                estado: 'En_proceso',
-                fecha: '27/11/2017 13:09:54'
-            },
-            {
-                folio: 'MEX-05-HANG-17',
-                estado: 'En_proceso',
-                fecha: '26/10/2017 07:12:33'
-            }
-        ];
+        activate();
+
+        function activate() {
+            SalePointRequests.getAll()
+                .then(function (listRequestsSuccess) {
+                    vm.allRequests = listRequestsSuccess;
+                    $log.debug(listRequestsSuccess);
+                })
+                .catch(function (listRequestsError) {
+                    $log.error(listRequestsError);
+                    toastr.error(Translate.translate('REQUESTS.LIST.TOASTR.ERROR'));
+                });
+        }
 
         function listRequests(requestKind) {
-            switch (requestKind) {
-                case 'Abierta':
-                    vm.requests = vm.pendingRequests;
-                    break;
-                case 'Atendida':
-                    vm.requests = vm.attendedRequests;
-                    break;
-                case 'Cancelada':
-                    vm.requests = vm.cancelledRequests;
-                    break;
-                case 'Cerrada':
-                    vm.requests = vm.closedRequests;
-                    break;
-                case 'En_proceso':
-                    vm.requests = vm.inProcessRequests;
-                    break;
-            }
-
+            vm.requests = _.findWhere(vm.requests, {status: requestKind});
         }
 
         function selectRequest(request) {
