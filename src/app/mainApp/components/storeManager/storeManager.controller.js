c12036000e9c48d491ed115fcfa89babcba548e9
Zed_Desktop <platae93@gmail.com>
2018-04-10 18:54:24 -0500

    Creación de archivos base para componente de establecimiento

---
+++ src/app/mainApp/components/storeManager/storeManager.controller.js
@@ -0,0 +1,24 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('app.mainApp')
+        .component('storeManager', {
+            templateUrl: 'app/mainApp/components/storeManager/storeManager.tmpl.html',
+            controller: storeManagerController,
+            bindings: {
+                canCreate: '<',
+                canModify: '<',
+                canDelete: '<',
+                canSearch: '<',
+                store: '<',
+                storeSelected: '&'
+            }
+        });
+
+    /* @ngInject */
+    function storeManagerController() {
+        var ctrl = this;
+    }
+
+})();
