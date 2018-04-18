ccd5c821e7664acc1c6d269e037ea9858b3a09f6
Zed_Desktop <platae93@gmail.com>
2018-04-18 09:20:42 -0500

    Se arregló barra de profile en bienvenida

--- src/app/mainApp/bienvenida/controllers/bienvenida.controller.js
+++ src/app/mainApp/bienvenida/controllers/bienvenida.controller.js
@@ -6,35 +6,12 @@
         .module('app.mainApp')
         .controller('bienvenidaController',bienvenidaController);
 
-    function bienvenidaController($scope,$rootScope){
+    function bienvenidaController(User, RoleStore){
         var vm = this;
 
-        vm.capturista=false;
-        vm.tecnico=false;
-        vm.cliente=false;
-        vm.Admin=false;
+        vm.user = User.getUser();
+        vm.roles = _.keys(RoleStore.getStore());
 
-        function showButtons(){
-            if (vm.role==="Administrador"){
-                vm.capturista=true;
-                vm.tecnico=true;
-                vm.cliente=true;
-                vm.admin=true;
-            }
-            else{
-                if(vm.role==="Capturista"){
-                    vm.capturista=true;
-                    vm.cliente=true;
-                }
-                else{
-                    if(vm.role==="Cliente"){
-                    vm.cliente=true;
-                }
-                    else
-                        vm.tecnico=true;
-                }
-            }
-        }
     }
 
 })();
