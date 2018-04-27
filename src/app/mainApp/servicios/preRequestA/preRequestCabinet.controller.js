38d4b0f7a0d253cb801ea1b2a5fbbba9ebeaeacf
alex <alexnm4194@gmail.com>
2018-04-27 02:09:05 -0500

    pantallas de prueba para los componentes de cabinet PV

---
+++ src/app/mainApp/servicios/preRequestA/preRequestCabinet.controller.js
@@ -0,0 +1,13 @@
+
+(function () {
+    angular
+        .module('app.mainApp.solicitudes')
+        .controller('NewCabinetPrerequestController', NewCabinetPrerequestController);
+
+    function NewCabinetPrerequestController(OPTIONS, URLS, udn, TipoEquipo, Helper, $mdEditDialog, $mdDialog)
+    {
+        var vm = this;
+
+    }
+
+})();
