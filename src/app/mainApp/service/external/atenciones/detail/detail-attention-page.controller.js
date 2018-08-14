35551e719cc91e7c9ae075794414dba5fb45a9ff
Lexus <erickllaureano@gmail.com>
2018-08-14 16:35:32 -0500

    se agrega el codigo necesario para listar y poder usar los cabinets en caso de alta o cambio

--- src/app/mainApp/service/external/atenciones/detail/detail-attention-page.controller.js
+++ src/app/mainApp/service/external/atenciones/detail/detail-attention-page.controller.js
@@ -7,7 +7,8 @@
 
     /* @ngInject */
     function DetailAttentionPageController($log, $state, $stateParams, toastr, Translate, SalePointRequests, Stores,
-                                         Persona_Admin, Geolocation, STORE_SEGMENTATION, SCORES, atencionPV) {
+                                         Persona_Admin, Geolocation, STORE_SEGMENTATION, SCORES, atencionPV,
+                                           cabinetPV, ErrorHandler, Helper, $scope, MarcaCabinet, ModeloCabinet) {
         var vm = this;
 
         //Function mapping
@@ -161,5 +162,196 @@
                 };
             });
         }
+
+
+        //codigo para listar cabinets
+        vm.todosprev = null;
+        vm.todos = [];
+        vm.todosSeleccionado = [];
+        vm.loadingPromise = null;
+        vm.limit = 20;
+        vm.offset = 0;
+        vm.searchText = '';
+        vm.cabinetSelected = {
+            economico: null,
+            no_serie: null,
+            marca: null,
+            modelo_id: null,
+            antiguedad: null
+        };
+        vm.marca = null;
+        vm.isSelected = false;
+        vm.isUsed = false;
+        vm.cabinetUsed = null;
+        var cabinetTemporal = null;
+
+        var models = null;
+
+        vm.listcabinets = listcabinets;
+        vm.selectCabinet = selectCabinet;
+        vm.filterCabinets = filterCabinets;
+        vm.cleanForm = cleanForm;
+        vm.listMarcas = listMarcas;
+        vm.listModelos = listModelos;
+        vm.changeTrademark = changeTrademark;
+        vm.seleccion = seleccion;
+        vm.accept = accept;
+        vm.unUsedCabinet = unUsedCabinet;
+
+        listMarcas();
+        listModelos();
+        listcabinets();
+
+
+        function unUsedCabinet() {
+            cabinetTemporal = null;
+            vm.isUsed = false;
+            cleanForm();
+        }
+
+        function seleccion() {
+            vm.todosSeleccionado = [];
+            console.log('cabinet a usar: ', cabinetTemporal);
+            vm.todosSeleccionado.push(cabinetTemporal);
+            vm.isUsed = true;
+        }
+
+        function accept() {
+            console.log('CREACION DE CABINET');
+            var aux = {
+                economico: vm.cabinetSelected.economico,
+                modelo_id: vm.cabinetSelected.modelo_id,
+                no_serie: vm.cabinetSelected.no_serie.toUpperCase(),
+                antiguedad: vm.cabinetSelected.antiguedad.toUpperCase()
+            };
+            cabinetPV.create(aux)
+                .then(function (res) {
+                    console.log(res);
+                    cabinetTemporal = res;
+                    ErrorHandler.succcesCreation();
+                })
+                .catch(function (err) {
+
+                    ErrorHandler.errortranslate(err);
+                });
+        }
+
+        function listMarcas(){
+            vm.loadingPromise = MarcaCabinet.listPromise(1000,0)
+                .then(function (res) {
+                    res = res.results;
+                    vm.marcas = Helper.filterDeleted(res, true);
+                })
+                .catch(function (err) {
+
+                });
+        }
+
+        function listModelos() {
+            vm.loadingPromise = ModeloCabinet.listWitout(1000,0)
+                .then(function (res) {
+                    res = res.results;
+                    models = Helper.filterDeleted(res, true);
+                })
+                .catch(function(err){
+
+                });
+        }
+
+        function changeTrademark() {
+            if (!vm.isSelected) {
+                vm.marca = vm.cabinetSelected.marca;
+            }
+            vm.modelos = null;
+            vm.modelos = _.where(models, {marca: parseInt(vm.marca)});
+        }
+
+
+        function selectCabinet(cabinet) {
+            console.log(cabinet);
+            vm.isSelected = true;
+            vm.marca = cabinet.modelo.marca;
+            cabinetTemporal = cabinet;
+            changeTrademark();
+            vm.cabinetSelected = {
+                economico: cabinet.economico,
+                no_serie: cabinet.no_serie,
+                marca: cabinet.modelo.marca,
+                modelo_id: cabinet.modelo_id,
+                antiguedad: cabinet.antiguedad
+            };
+        }
+
+        function cleanForm() {
+            vm.modelos = null;
+            vm.cabinetSelected = {
+                economico: null,
+                no_serie: null,
+                marca: null,
+                modelo_id: null,
+                antiguedad: null
+            };
+            vm.isSelected = false;
+            $scope.newcabinetFrom.$setPristine();
+            $scope.newcabinetFrom.$setUntouched();
+        }
+
+        function filterCabinets() {
+            vm.todos = [];
+            vm.todos = _.filter(vm.todosprev, function (cabinet) {
+                if (!angular.isString(vm.searchText) || vm.searchText === ''){
+                    return true;
+                }
+                return cabinet.economico.toLowerCase().indexOf(vm.searchText.toLowerCase()) >= 0;
+            });
+        }
+
+        function listcabinets(){
+            var ux = "Activo";
+
+            vm.loadingPromise = cabinetPV.list(1000, 0)
+                .then(function (res) {
+                    res = res.results;
+                    console.log(res);
+                    vm.todosprev = Helper.filterDeleted(res, true);
+
+                    angular.forEach(vm.todosprev, function (cabinet) {
+
+                        if(cabinet.activo === true){
+                            ux = "Activo";
+                        }else{
+                            ux = "Inactivo";
+                        }
+
+                        var cabinetPreview = {
+                            economico: cabinet.economico,
+                            modelo: {
+                                id: cabinet.modelo.id,
+                                deleted: cabinet.modelo.deleted,
+                                nombre: cabinet.modelo.nombre,
+                                descripcion: cabinet.modelo.descripcion,
+                                palabra_clave: cabinet.modelo.palabra_clave,
+                                tipo: cabinet.modelo.tipo,
+                                marca: cabinet.modelo.marca
+                            },
+                            modelo_id: cabinet.modelo_id,
+                            antiguedad: cabinet.antiguedad,
+                            activo: cabinet.activo,
+                            estado: ux,
+                            no_incidencias: cabinet.no_incidencias,
+                            qr_code: cabinet.qr_code,
+                            deleted: cabinet.deleted,
+                            no_serie: cabinet.no_serie
+                        };
+
+                        vm.todos.push(cabinetPreview);
+                    });
+                })
+                .catch(function (err) {
+
+                });
+        }
+        //codigo para listar cabinets
+
     }
 })();
