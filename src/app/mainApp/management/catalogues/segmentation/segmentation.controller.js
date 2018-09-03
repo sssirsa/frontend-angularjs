5a68827e3b815166929e2558d2409829ecef2f20
alex <alexnm4194@gmail.com>
2018-09-03 15:50:36 -0500

    Se agregon el demo al menu. Se implemento el componente generico al catalogo de Segmentación.

--- src/app/mainApp/management/catalogues/segmentation/segmentation.controller.js
+++ src/app/mainApp/management/catalogues/segmentation/segmentation.controller.js
@@ -4,212 +4,150 @@
     angular
         .module('app.mainApp.management.catalogues')
         .controller('segmentationController', segmentationController)
-        .filter('lineaSegmentationSearch', custom);
 
     /* @ngInject */
-    function segmentationController(Segmentation, Helper, $scope, toastr, Translate, $mdDialog) {
+    function segmentationController(URLS, Translate) {
 
         var vm = this;
 
-        vm.cancel = cancel;
-        vm.create = create;
-        vm.restore = restore;
-        vm.remove = remove;
-        vm.update = update;
+        vm.url = URLS.segmentation;
+        vm.kind = 'Mobile';
+        vm.name = Translate.translate('SEGMENTATION.LABELS.TITLE');
 
-        vm.lookup = lookup;
-        vm.querySearch = querySearch;
-        vm.selectedLineas = selectedLineas;
-        vm.selectedItemChange = selectedItemChange;
-        vm.toggleDeletedFunction = toggleDeletedFunction;
+        //Labels
+        vm.totalText = 'Total de elementos';
+        vm.totalFilteredText = 'Elementos encontrados';
 
-        var segmentationLocal = null;
+        //Button labels
+        vm.searchButtonText = 'Buscar Segmentación';
+        vm.createButtonText = 'Crear Segmentación';
+        vm.deleteButtonText = 'Borrar Segmentación';
+        vm.modifyButtonText = 'Editar Segmentación';
+        vm.nextButtonText = 'Siguiente';
+        vm.previousButtonText = 'Anterior';
+        vm.loadMoreButtonText = 'Cargar mas segmentaciones';
+        vm.removeFilterButtonText = 'Qutar filtro';
 
-        vm.search_items = [];
-        vm.searchText = '';
-        vm.segmentation = angular.copy(segmentationLocal);
-        vm.delete_or_edit = false;
-        vm.numberBuffer = '';
-        vm.myHeight = window.innerHeight - 250;
-        vm.myStyle = {"min-height": "" + vm.myHeight + "px"};
-        vm.toggleDeleted = true;
-        vm.minicolorSettings = {
-            control: 'hue',
-            format: 'hex',
-            letterCase: 'uppercase',
-            position: 'bottom left'
-        };
+        //Messages
+        vm.loadingMessage = 'Cargando Segmentaciones';
 
+        //Functions
+        vm.onElementSelect = onElementSelect;
 
-
-        activate();
-        init();
-
-        function init() {
-            vm.successTitle = Translate.translate('MAIN.MSG.SUCCESS_TITLE');
-            vm.errorTitle = Translate.translate('MAIN.MSG.ERROR_TITLE');
-            vm.successCreateMessage = Translate.translate('MAIN.MSG.GENERIC_SUCCESS_CREATE');
-            vm.errorMessage = Translate.translate('MAIN.MSG.ERROR_MESSAGE');
-            vm.successUpdateMessage = Translate.translate('MAIN.MSG.GENERIC_SUCCESS_UPDATE');
-            vm.successDeleteMessage = Translate.translate('MAIN.MSG.GENERIC_SUCCESS_DELETE');
-            vm.deleteButton = Translate.translate('MAIN.BUTTONS.DELETE');
-            vm.cancelButton = Translate.translate('MAIN.BUTTONS.CANCEL');
-            vm.dialogTitle = Translate.translate('MAIN.DIALOG.DELETE_TITLE');
-            vm.dialogMessage = Translate.translate('MAIN.DIALOG.DELETE_MESSAGE');
-            vm.duplicateMessage = Translate.translate('TRANSPORT_LINE.FORM.LABEL.DUPLICATE');
-            vm.dialogRestoreTitle = Translate.translate('MAIN.DIALOG.RESTORE_TITLE');
-            vm.dialogRestoreMessage = Translate.translate('MAIN.DIALOG.RESTORE_MESSAGE');
-            vm.restoreButton = Translate.translate('MAIN.BUTTONS.RESTORE');
-            vm.successRestoreMessage = Translate.translate('MAIN.MSG.GENERIC_SUCCESS_RESTORE');
-        }
-
-
-        function activate() {
-            listlineas();
-        }
-
-        function toggleDeletedFunction() {
-            listlineas();
-            cancel();
-        }
-
-        function listlineas() {
-            vm.loadingPromise = Segmentation.list().then(function (res) {
-                vm.lineas = Helper.filterDeleted(res, vm.toggleDeleted);
-                vm.lineas = _.sortBy(vm.lineas, 'nombre');
-            }).catch(function (err) {
-
-            });
-        }
-
-        function remove(ev) {
-            var confirm = $mdDialog.confirm()
-                .title(vm.dialogTitle)
-                .textContent(vm.dialogMessage)
-                .ariaLabel('Confirmar eliminación')
-                .ok(vm.deleteButton)
-                .cancel(vm.cancelButton);
-            $mdDialog.show(confirm).then(function () {
-                vm.formLoading = Segmentation.remove(vm.segmentation.id).then(function (res) {
-                    toastr.success(vm.successDeleteMessage, vm.successTitle);
-                    cancel();
-                    activate();
-                }).catch(function (res) {
-                    toastr.warning(vm.errorMessage, vm.errorTitle);
-                });
-            }, function () {
-
-            });
-        }
-
-        function update() {
-            vm.formLoading = Segmentation.update(vm.segmentation, vm.segmentation.id).then(function (res) {
-                toastr.success(vm.successUpdateMessage, vm.successTitle);
-                cancel();
-                activate();
-            }).catch(function (err) {
-                console.log(err);
-                if (err.status == 400 && err.data.nombre != undefined) {
-                    toastr.error(vm.duplicateMessage, vm.errorTitle);
-                } else {
-                    toastr.error(vm.errorMessage, vm.errorTitle);
+        //Actions meta
+        vm.actions = {
+            POST: {
+                fields: [
+                    {
+                        type: 'text',
+                        model: 'nombre',
+                        label: 'Nombre',
+                        required: true,
+                        validations: {
+                            errors: {
+                                required: 'El nombre del estado es obligatorio'
+                            }
+                        }
+                    },
+                    {
+                        type: 'text',
+                        model: 'descripcion',
+                        label: 'Descripcion',
+                        required: false
+                    },
+                    {
+                        type: 'color',
+                        model: 'color',
+                        label: 'Color',
+                        required: true,
+                        validations: {
+                            errors: {
+                                required: 'El color es obligatorio'
+                            }
+                        }
+                    }
+                ],
+                dialog: {
+                    title: 'Crear segmentación',
+                    okButton: Translate.translate('MAIN.BUTTONS.SUBMIT'),
+                    cancelButton: Translate.translate('MAIN.BUTTONS.CANCEL'),
+                    loading: 'Creando Segmentación'
                 }
-            });
-        }
-
-        function create() {
-            vm.formLoading = Segmentation.create(vm.segmentation).then(function (res) {
-                toastr.success(vm.successCreateMessage, vm.successTitle);
-                vm.segmentation = angular.copy(segmentationLocal);
-                cancel();
-                activate();
-            }).catch(function (err) {
-                console.log(err);
-                if (err.status == 400 && err.data.codigo_estado != undefined) {
-                    toastr.error(vm.duplicateMessage, vm.errorTitle);
-                } else {
-                    toastr.error(vm.errorMessage, vm.errorTitle);
+            },
+            PUT: {
+                fields: [],
+                dialog: {
+                    title: 'Editar segmentación',
+                    okButton: Translate.translate('MAIN.BUTTONS.SUBMIT'),
+                    cancelButton: Translate.translate('MAIN.BUTTONS.CANCEL'),
+                    loading: 'Guardando Segmentación'
                 }
-            });
-        }
-
-        function restore() {
-            var confirm = $mdDialog.confirm()
-                .title(vm.dialogRestoreTitle)
-                .textContent(vm.dialogRestoreMessage)
-                .ariaLabel('Confirmar restauración')
-                .ok(vm.restoreButton)
-                .cancel(vm.cancelButton);
-            $mdDialog.show(confirm).then(function () {
-                vm.segmentation.deleted = false;
-                vm.formLoading = Segmentation.update(vm.segmentation).then(function (res) {
-                    toastr.success(vm.successRestoreMessage, vm.successTitle);
-                    cancel();
-                    activate();
-                }).catch(function (res) {
-                    vm.segmentation.deleted = true;
-                    toastr.warning(vm.errorMessage, vm.errorTitle);
-                });
-            }, function () {
-
-            });
-        }
-
-        function cancel() {
-            vm.segmentation = angular.copy(segmentationLocal);
-            vm.delete_or_edit = false;
-            vm.selectedLineaList = null;
-            vm.numberBuffer = null;
-            vm.searchText = null;
-
-
-            $scope.SegmentationForm.$setPristine();
-            $scope.SegmentationForm.$setUntouched();
-        }
-
-
-
-        function selectedItemChange(item) {
-            if (item != null) {
-                vm.segmentation = angular.copy(item);
-                vm.delete_or_edit = vm.segmentation.deleted;
-            } else {
-                cancel();
+            },
+            DELETE: {
+                id: 'id',
+                dialog: {
+                    title: 'Eliminar segmentación',
+                    message: 'Confirme la eliminación de la segmentación',
+                    okButton: Translate.translate('MAIN.BUTTONS.SUBMIT'),
+                    cancelButton: Translate.translate('MAIN.BUTTONS.CANCEL'),
+                    loading: 'Eliminando Segmentación'
+                }
+            },
+            LIST: {
+                elements: 'results',
+                mode: 'infinite',
+                pagination: {
+                    total: 'count'
+                },
+                fields: [
+                    {
+                        type: 'text',
+                        model: 'nombre',
+                        label: 'Nombre'
+                    },
+                    {
+                        type: 'text',
+                        model: 'descripcion',
+                        label: 'Descripcion'
+                    },
+                    {
+                        type: 'color',
+                        model: 'color',
+                        label: 'Color'
+                    }
+                ],
+                softDelete: {
+                    hide: 'deleted',
+                    reverse: false
+                }
+            },
+            SEARCH: {
+                dialog: {
+                    title: 'Busqueda de Segmentación',
+                    searchButton: 'Buscar',
+                    loadingText: 'Buscando segmentación'
+                },
+                filters: [
+                    {
+                        type: 'istartswith',
+                        model: 'nombre',
+                        header: 'por Nombre',
+                        label: 'Nombre',
+                        field: {
+                            type: 'text'
+                        }
+                    }
+                ]
             }
         }
 
-        function selectedLineas(project) {
-            vm.selectedLineaList = project;
-            vm.segmentation = angular.copy(project);
-            vm.delete_or_edit = vm.segmentation.deleted;
+        function onElementSelect(element) {
+            //Here goes the handling for element selection, such as detail page navigation
+            console.debug('Element selected');
+            console.debug(element);
+            console.log(element);
         }
 
-        function querySearch(query) {
-            var results = query ? lookup(query) : vm.lineas;
-            return results;
-
-        }
-
-        function lookup(search_text) {
-            vm.search_items = _.filter(vm.lineas, function (item) {
-                return item.nombre.toLowerCase().indexOf(search_text.toLowerCase()) >= 0;
-            });
-            return vm.search_items;
-        }
-
-    }
-
-    function custom() {
-        return function (input, text) {
-            if (!angular.isString(text) || text === '') {
-                return input;
-            }
-
-            return _.filter(input, function (item) {
-                return item.nombre.toLowerCase().indexOf(text.toLowerCase()) >= 0;
-            });
-
-        };
     }
 
 })();
