5ec647d2afee6c5621b48100a83af1945d7ae77a
Erick Laureano <erickllaureano@gmail.com>
2018-09-07 19:12:16 -0500

    se agrega la logica de filtrado y paginado al modulo de presolicitud

--- src/app/mainApp/service/external/preRequests/preRequest.controller.js
+++ src/app/mainApp/service/external/preRequests/preRequest.controller.js
@@ -4,41 +4,74 @@
     angular
         .module('app.mainApp.service')
         .controller('preRequestListController',preRequestListController);
-    function preRequestListController(preRequests) {
+    function preRequestListController(preRequests, toastr, Translate) {
 
         var vm = this;
         //Listado de Variables
 
         vm.list=[];
+        vm.offset = 0;
+        vm.filteredActivated = false;
+        vm.limit = 5;
+        vm.lastFilter = 'Todo';
+        vm.lastKindFilter = 'Todo';
 
         //Listado de funciones
 
         vm.listpreRequests=listpreRequests;
         vm.listFilteredpreRequests=listFilteredpreRequests;
+        vm.sig = sigPage;
+        vm.prev = prevPage;
         listpreRequests();
 
         function listpreRequests() {
-            var promiseListadoPreRequests=preRequests.getAll();
-            promiseListadoPreRequests.then(function(listprerequestelements){
-                vm.list=listprerequestelements;
-                vm.requests = vm.list;
-                console.log(vm.list);
-            }).catch(function (errCarga) {
-                console.log(errCarga);
-
-            });
+            vm.loadingPromise = preRequests.getAll(vm.limit, vm.offset)
+                .then(function(listprerequestelements){
+                    vm.list=listprerequestelements;
+                    prepareDataFunction();
+                })
+                .catch(function (errCarga) {
+                    toastr.error(Translate.translate('REQUESTS.LIST.TOASTR.ERROR'));
+                });
 
         }
         function listFilteredpreRequests(requestKind) {
-            if (requestKind !== 'Todo') {
-                vm.requests = _.where(vm.list, {status: requestKind});
+            vm.filteredActivated = true;
+            vm.lastFilter = requestKind;
+            if (vm.lastKindFilter !== requestKind){
+                vm.offset = 0;
+                vm.lastKindFilter = requestKind;
+            }
+            if (requestKind === 'Todo') {
+                listpreRequests();
             }
             else {
-                vm.requests = vm.list;
+                var filterSTR = 'status='+requestKind;
+                vm.loadingPromise = preRequests.getAll(vm.limit, vm.offset, filterSTR)
+                    .then(function(listprerequestelements){
+                        vm.list=listprerequestelements;
+                        prepareDataFunction();
+                    })
+                    .catch(function (errCarga) {
+                        toastr.error(Translate.translate('REQUESTS.LIST.TOASTR.ERROR'));
+                    });
             }
         }
 
+        function prepareDataFunction() {
+            vm.requests = vm.list.results;
+            vm.filteredActivated = false;
+        }
 
+        function sigPage() {
+            vm.offset += vm.limit;
+            listFilteredpreRequests(vm.lastFilter);
+        }
+
+        function prevPage() {
+            vm.offset -= vm.limit;
+            listFilteredpreRequests(vm.lastFilter);
+        }
 
     }
 })();
