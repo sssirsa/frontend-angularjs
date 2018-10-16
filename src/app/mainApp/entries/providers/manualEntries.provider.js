e0208e822f92b718fc9f2f6fe7a9ea2bf7ddccaf
ZedElric@Inspiron15/5000 <platae@gmail.com>
2018-10-15 21:35:21 -0500

    Implementación de catálogos en entrada de garantías

--- src/app/mainApp/entries/providers/manualEntries.provider.js
+++ src/app/mainApp/entries/providers/manualEntries.provider.js
@@ -3,8 +3,13 @@
         .module('app.mainApp.entries')
         .service('MANUAL_ENTRIES', ManualEntriesProvider);
 
-    function ManualEntriesProvider(WebRestangular, $q, URLS) {
-        const baseUrl = WebRestangular.all(URLS.manualEntries);//Modify the URL when known
+    function ManualEntriesProvider(
+        WebRestangular,
+        $q,
+        URLS,
+        Translate
+    ) {
+        const baseUrl = WebRestangular.all(URLS.manualEntries);
         const urls = URLS.entries;
 
         function createNew(element) {
@@ -35,12 +40,159 @@
             return baseUrl.all(urls.close).all(id).customPUT(element);
         }
 
+        const warrantyEntry = {
+            template: function template() {
+                var template = {
+                    tipo_entrada: 'Garantias',
+                    cabinets: []
+                }
+                return template;
+            },
+            catalogues: function catalogues() {
+                var catalogues = {
+                    subsidiary: {
+                        catalog: {
+                            url: URLS.sucursal,
+                            kind: 'Web',
+                            name: Translate.translate('ENTRIES.WARRANTY.LABELS.SUBSIDIARY'),
+                            loadMoreButtonText: Translate.translate('MAIN.BUTTONS.LOAD_MORE'),
+                            model: 'id',
+                            option: 'nombre'
+                        },
+                        hint: Translate.translate('ENTRIES.WARRANTY.HINTS.SUBSIDIARY'),
+                        icon:'fa fa-home',
+                        required: true,
+                        pagination: {
+                            total: 'count',
+                            next: 'next'
+                        },
+                        elements: 'results',
+                        softDelete: {
+                            hide: 'deleted',
+                            reverse: false
+                        }
+                    },
+                    transport_line: {
+                        catalog: {
+                            url: URLS.linea_transporte,
+                            kind: 'Web',
+                            name: Translate.translate('ENTRIES.WARRANTY.LABELS.TRANSPORT_LINE'),
+                            loadMoreButtonText: Translate.translate('MAIN.BUTTONS.LOAD_MORE'),
+                            model: 'id',
+                            option: 'razon_social'
+                        },
+                        hint: Translate.translate('ENTRIES.WARRANTY.HINTS.TRANSPORT_LINE'),
+                        icon:'fa fa-shuttle-van',
+                        required: true,
+                        pagination: {
+                            total: 'count',
+                            next: 'next'
+                        },
+                        elements: 'results',
+                        softDelete: {
+                            hide: 'deleted',
+                            reverse: false
+                        }
+                    },
+                    transport_kind: {
+                        catalog: {
+                            url: URLS.tipo_transporte,
+                            kind: 'Web',
+                            name: Translate.translate('ENTRIES.WARRANTY.LABELS.TRANSPORT_KIND'),
+                            loadMoreButtonText: Translate.translate('MAIN.BUTTONS.LOAD_MORE'),
+                            model: 'id',
+                            option: 'descripcion'
+                        },
+                        hint: Translate.translate('ENTRIES.WARRANTY.HINTS.TRANSPORT_KIND'),
+                        icon:'fa fa-truck',
+                        required: true,
+                        pagination: {
+                            total: 'count',
+                            next: 'next'
+                        },
+                        elements: 'results',
+                        softDelete: {
+                            hide: 'deleted',
+                            reverse: false
+                        }
+                    },
+                    udn: {
+                        catalog: {
+                            url: URLS.udn,
+                            kind: 'Web',
+                            name: Translate.translate('ENTRIES.WARRANTY.LABELS.AGENCY'),
+                            loadMoreButtonText: Translate.translate('MAIN.BUTTONS.LOAD_MORE'),
+                            model: 'id',
+                            option: 'agencia'
+                        },
+                        hint: Translate.translate('ENTRIES.WARRANTY.HINTS.AGENCY'),
+                        icon: 'fa fa-building',
+                        required: true,
+                        pagination: {
+                            total: 'count',
+                            next: 'next'
+                        },
+                        elements: 'results',
+                        softDelete: {
+                            hide: 'deleted',
+                            reverse: false
+                        }
+                    },
+                    project: {
+                        catalog: {
+                            url: URLS.proyecto,
+                            kind: 'Web',
+                            name: Translate.translate('ENTRIES.WARRANTY.LABELS.PROJECT'),
+                            loadMoreButtonText: Translate.translate('MAIN.BUTTONS.LOAD_MORE'),
+                            model: 'id',
+                            option: 'descripcion'
+                        },
+                        hint: Translate.translate('ENTRIES.WARRANTY.HINTS.PROJECT'),
+                        icon: 'fa fa-book',
+                        required: true,
+                        pagination: {
+                            total: 'count',
+                            next: 'next'
+                        },
+                        elements: 'results',
+                        softDelete: {
+                            hide: 'deleted',
+                            reverse: false
+                        }
+                    }
+                };
+                return catalogues;
+            }
+        };
+
+        const newEntry = {
+            tipo_entrada: 'Nuevos',
+            cabinets: []
+        };
+
+        const unrecognizableEntry = {
+            tipo_entrada: 'No_Capitalizados',
+            cabinets: []
+        };
+
+        const obsoleteEntry = {
+            tipo_entrada: 'Obsoletos',
+            cabinets: []
+        };
+
         return {
             createNew: createNew,
             createWarranty: createWarranty,
             createObsolete: createObsolete,
             createUnrecognizable: createUnrecognizable,
-            addCabinet: addCabinet
+            addCabinet: addCabinet,
+            detail: detail,
+            close: close,
+            //Constants
+            warrantyEntry: warrantyEntry,
+            newEntry: newEntry,
+            obsoleteEntry: obsoleteEntry,
+            unrecognizableEntry: unrecognizableEntry
         }
 
     }
--- src/app/mainApp/entries/warranty/manual/warrantyManualEntry.controller.js
+++ src/app/mainApp/entries/warranty/manual/warrantyManualEntry.controller.js
@@ -2,7 +2,33 @@
     angular
         .module('app.mainApp.entries.warranty')
         .controller('warrantyManualEntryController', WarrantyManualEntryController);
-    function WarrantyManualEntryController() {
+    function WarrantyManualEntryController(
+        MANUAL_ENTRIES,
+        User,
+        URLS
+    ) {
+        var vm = this;
+
+        //Variables
+        vm.entry = {};
+        vm.showSubsidiarySelector = false;
+        vm.catalogues = {};
+
+        // Auto invoked init function
+        (function init() {
+            vm.entry = MANUAL_ENTRIES.warrantyEntry.template();
+            vm.catalogues = MANUAL_ENTRIES.warrantyEntry.catalogues();
+            //Determining whether or not to show the Subsidiary selector.
+            if (User.getUser().hasOwnProperty('sucursal')) {
+                vm.showSubsidiarySelector = !User.getUser().sucursal;
+            }
+        })();
+
+        //Controller functions
+
+        vm.onElementSelect = function (element, field) {
+            vm.entry[field] = element;
+        }
 
     }
 
