8ec2733c078c471da82c6e3636dbb362697b8d0f
alex <alexnm4194@gmail.com>
2018-10-23 18:01:02 -0500

    Se creo una nueva pantalla para el manejo de los cabinets Se translado el modal de creación de cabinet a gestion de cabinets

--- src/app/mainApp/inventory/inventory.config.js
+++ src/app/mainApp/inventory/inventory.config.js
@@ -7,7 +7,7 @@
     function moduleConfig($stateProvider, $translatePartialLoaderProvider, triMenuProvider) {
         $translatePartialLoaderProvider.addPart('app/mainApp/inventory');
         $stateProvider
-            .state('triangular.admin-default.cabinets', {
+            /*.state('triangular.admin-default.cabinets', {
                 url: '/cabinets',
                 data: {
                     permissions: {
@@ -17,7 +17,7 @@
                 templateUrl: 'app/mainApp/inventory/cabinet/cabinet.tmpl.html',
                 controller: 'cabinetController',
                 controllerAs: 'vm'
-            })
+            })*/
             .state('triangular.admin-default.insumos', {
                 url: '/insumos',
                 data: {
@@ -27,6 +27,28 @@
                 },
                 templateUrl: 'app/mainApp/inventory/insumo/insumo.tmpl.html',
                 controller: 'insumoController',
+                controllerAs: 'vm'
+            })
+            .state('triangular.admin-default.no_capitalizado', {
+                url: '/no_capitalizado',
+                data: {
+                    permissions: {
+                        only: ['ADMINISTRADOR', 'CAPTURISTA']
+                    }
+                },
+                templateUrl: 'app/mainApp/inventory/noCapitalizado/noCapitalizado.tmpl.html',
+                controller: 'noCapitalizadoController',
+                controllerAs: 'vm'
+            })
+            .state('triangular.admin-default.gestion_cabinets', {
+                url: '/cabinet',
+                data: {
+                    permissions: {
+                        only: ['ADMINISTRADOR', 'CAPTURISTA']
+                    }
+                },
+                templateUrl: 'app/mainApp/inventory/gestionCabinet/cabinetGestion.tmpl.html',
+                controller: 'cabinetGestionController',
                 controllerAs: 'vm'
             });
 
@@ -38,12 +60,20 @@
                 permission: ['ADMINISTRADOR', 'CAPTURISTA'],
                 priority: 8,
                 children: [{
+                    name: 'MAIN.MENU.INVENTORY.CABINET_MANAGEMENT',
+                    state: 'triangular.admin-default.gestion_cabinets',
+                    type: 'link'
+                }/*, {
                     name: 'MAIN.MENU.INVENTORY.CABINETS',
                     state: 'triangular.admin-default.cabinets',
                     type: 'link'
-                }, {
+                }*/, {
                     name: 'MAIN.MENU.INVENTORY.CONSUMABLES',
                     state: 'triangular.admin-default.insumos',
+                    type: 'link'
+                }, {
+                    name: 'MAIN.MENU.INVENTORY.NO_CAPITALIZADO',
+                    state: 'triangular.admin-default.no_capitalizado',
                     type: 'link'
                 }
                 ]
