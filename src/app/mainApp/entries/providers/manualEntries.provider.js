188563b27bba61a33c2a54f51e9e346133fe6e24
ZedElric@Inspiron15/5000 <platae@gmail.com>
2018-11-16 15:59:11 -0600

    Actualización de provider de entradas
    -Se añadió la propiedad "entries" para apuntar a las urls segun los tipos de entrada

--- src/app/mainApp/entries/providers/manualEntries.provider.js
+++ src/app/mainApp/entries/providers/manualEntries.provider.js
@@ -14,31 +14,31 @@
         const urls = URLS.entries;
 
         function createNew(element) {
-            return baseUrl.all(urls.new).customPOST(element);
+            return baseUrl.all(urls.entries.new).customPOST(element);
         }
 
         function createWarranty(element) {
-            return baseUrl.all(urls.warranty).customPOST(element);
+            return baseUrl.all(urls.entries.warranty).customPOST(element);
         }
 
         function createObsolete(element) {
-            return baseUrl.all(urls.obsolete).customPOST(element);
+            return baseUrl.all(urls.entries.obsolete).customPOST(element);
         }
 
         function createUnrecognizable(element) {
-            return baseUrl.all(urls.unrecognizable).customPOST(element);
+            return baseUrl.all(urls.entries.unrecognizable).customPOST(element);
         }
 
         function addCabinet(id, element) {
-            return baseUrl.all(urls.addCabinet).all(id).customPUT(element);
+            return baseUrl.all(urls.entries.addCabinet).all(id).customPUT(element);
         }
 
         function detail(id) {
-            return baseUrl.all(urls.close).all(id).customGET();
+            return baseUrl.all(urls.entries.close).all(id).customGET();
         }
 
         function close(id, element) {
-            return baseUrl.all(urls.close).all(id).customPUT(element);
+            return baseUrl.all(urls.entries.close).all(id).customPUT(element);
         }
 
         function getCabinet(id) {
