61bcc6565d4851af05c7521a562f50f5b816e1c9
ZedElric@Inspiron15/5000 <platae@gmail.com>
2018-11-21 15:28:52 -0600

    Actualización inicial de constants

--- src/app/app.constants.js
+++ src/app/app.constants.js
@@ -391,8 +391,8 @@
 
             ],
             salePointAssignKind: [
-                {id: 'pending', value: 'Pendientes'},
-                {id: 'all', value: 'Detalle de atención'}
+                { id: 'pending', value: 'Pendientes' },
+                { id: 'all', value: 'Detalle de atención' }
             ],
             requestKinds: [
                 {
@@ -418,6 +418,24 @@
             ]
         })
         .constant('URLS', {
+            //New module separation
+            entries_departures: {
+                base: '' //TODO: Update when known
+            },
+            genesis: {
+                base: 'genesis'
+            },
+            management: {
+                base: '' //TODO: Update when known
+            },
+            mobile: {
+                base:'mobile'
+            },
+            technical_service: {
+                base: '' //TODO: Update when known
+            },
+            //TODO: Re group under the proper category
+            //When done, delete all below this line
             geoLocation: 'https://www.google.com/maps/search/?api=1&query=',
             condicion: 'condition',
             estatus_unilever: 'status_unilever',
