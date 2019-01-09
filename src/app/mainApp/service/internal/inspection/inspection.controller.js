e1b51c72d2bf11b7fe68290b1f3841654b2f8a64
franciscojaviercerdamartinez <fco.cerda.mtz@gmail.com>
2019-01-09 17:23:04 -0600

    Agregada interfaz y diseño de la inspeccion del cabinet

--- src/app/mainApp/service/internal/inspection/inspection.controller.js
+++ src/app/mainApp/service/internal/inspection/inspection.controller.js
@@ -7,8 +7,29 @@
     angular
         .module('app.mainApp.service')
         .controller('inspectionController', inspectionController);
-    function inspectionController() {
+    function inspectionController($scope, Translate, toastr, cabinetUC, ErrorHandler) {
         var vm = this;
+        vm.asset = {};//objeto contenedor del cabinet
+        vm.asset_id=''; //asset identifier
+        vm.title_info=Translate.translate('INSPECTION.GENERAL_INFO');
+        vm.assets_info=Translate.translate('INSPECTION.BULK_ASSETS');
+
+        vm.search_asset = search_asset;
+        function search_asset() {
+            //Search in cabinets location
+            var promiseCabinetInfo = cabinetUC.getByID(vm.asset_id);
+            promiseCabinetInfo.then(function (asset) {
+                vm.asset = asset;
+                //selection subsidiary
+                console.log(vm.asset);
+
+
+            }).catch(function (errormsg) {
+                ErrorHandler.errorTranslate(errormsg);
+            });
+
+        }
+
 
     }
 
