9972d7f9c4c772dbdb4b68be083f29b92e50d302
franciscojaviercerdamartinez <fco.cerda.mtz@gmail.com>
2019-01-16 15:38:18 -0600

    Agregado Manejo de  Stocks Agregado Manejo de No stock TODO SELECCION DE INSUMO CON CANTIDAD PARA ENVIARLO AL COMPONENTE CONTENEDOR

--- src/app/mainApp/service/internal/components/bulkAssets/bullkAssets.controller.js
+++ src/app/mainApp/service/internal/components/bulkAssets/bullkAssets.controller.js
@@ -9,15 +9,58 @@
             templateUrl: 'app/mainApp/service/internal/components/bulkAssets/bulkAssets.tmpl.html',
             controller: bulkAssetsController,
             bindings: {
-                max_stock: '<',
-                bulkAsset:'<',
-                bulkAssetSelected: '&'
+                bulkAsset: '<',
+                bulkAssetSelected: '&',
+                sucursal: '<'
             }
         });
 
-    function bulkAssetsController(){
+    function bulkAssetsController() {
 
         var vm = this;
+        vm.finalAssetBulk = {};
+        vm.stockHere = {};
+        //insumo_lote_id
+        activate();
+        function activate() {
+
+
+        }
+
+        function getMaxValue() {
+            var stock = Number(getStock());
+            var used = Number(vm.bulkAsset.cantidad);
+            if (used < 1) {
+                vm.showSelector=true;
+                if (stock < used) {
+                    if(stock>0) {
+                        vm.maxUseAccepted = stock;
+                    }
+                    else{
+                        vm.notStock=true;
+                    }
+                } else {
+                    vm.maxUseAccepted = used;
+                }
+            }else{
+                if (stock < used) {
+                    vm.notStock=true;
+                } else {
+                    vm.finalAssetBulk.cantidad = used;
+                }
+            }
+
+        }
+
+        function getStock() {
+
+
+            var stock = vm.bulkAsset.catalogo_insumo_lote.stock.filter(function (element) {
+                return element.sucursal.id === 1;
+            });
+            return stock.cantidad;
+
+        }
 
 
     }
