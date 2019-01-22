0f3c2947412704fab8f9e0baeb26c129c6f844ff
franciscojaviercerdamartinez <fco.cerda.mtz@gmail.com>
2019-01-22 08:37:54 -0600

    componente que obtiene el cabinet y la información de la etapa del mismo
    TODO:
    Obtener la etapa del mismo e importar y usar componente que muestre la información inicial

---
+++ src/app/mainApp/service/internal/components/searchCabinetStep/searchCabinetStep.cotroller.js
@@ -0,0 +1,44 @@
+/**
+ * Created by franciscojaviercerdamartinez on 1/21/19.
+ */
+(function () {
+
+    angular
+        .module('app.mainApp')
+        .component('searchCabinetStep', {
+            templateUrl: 'app/mainApp/service/internal/components/searchCabinetStep/searchCabinetStep.tmpl.html',
+            controller: searchCabinetStepController,
+            bindings: {
+
+                onSelect: '&',
+
+            }
+        });
+    function searchCabinetStepController($scope, Translate, toastr, cabinetUC, ErrorHandler) {
+        var vm = this;
+        vm.asset = {};//objeto contenedor del cabinet
+        vm.asset_id = ''; //asset identifier
+        vm.title_info = Translate.translate('INSPECTION.GENERAL_INFO');
+        vm.assets_info = Translate.translate('INSPECTION.BULK_ASSETS');
+
+        vm.search_asset = search_asset;
+        function search_asset() {
+            //Search in cabinets location
+            var promiseCabinetInfo = cabinetUC.getByID(vm.asset_id);
+            promiseCabinetInfo.then(function (asset) {
+                vm.asset = asset;
+                //selection subsidiary
+                console.log(vm.asset);
+
+
+            }).catch(function (errormsg) {
+                ErrorHandler.errorTranslate(errormsg);
+            });
+
+        }
+
+
+    }
+
+
+})();
