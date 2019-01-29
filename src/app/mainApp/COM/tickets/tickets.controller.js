6b1d57d9e54cccf2dc70455a421f9dba77313dbf
alex <alexnm4194@gmail.com>
2019-01-29 13:54:01 -0600

    creación de modulo de tickets

---
+++ src/app/mainApp/COM/tickets/tickets.controller.js
@@ -0,0 +1,16 @@
+/** Created by Alejandro Noriega on 29/01/19 */
+
+(function () {
+    'use strict';
+
+    angular
+        .module('app.mainApp.com.tickets')
+        .controller('ticketsController', ticketsController);
+
+    function ticketsController() {
+        var vm = this;
+
+    }
+
+
+})();
