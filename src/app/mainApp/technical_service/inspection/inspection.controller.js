498c785be820634a4360845ecbeec853377115a5
franciscojaviercerdamartinez <fco.cerda.mtz@gmail.com>
2019-02-12 16:04:14 -0600

    obtener stickers, Cambiado todos los componentes y la pantralla de  inspeccion al modulo correcto creado por  plata,
    TODO.
     Provider para enviar inspecciones y

--- src/app/mainApp/technical_service/inspection/inspection.controller.js
+++ src/app/mainApp/technical_service/inspection/inspection.controller.js
@@ -7,13 +7,39 @@
     angular
         .module('app.mainApp.service')
         .controller('inspectionController', inspectionController);
-    function inspectionController($scope, Translate, ErrorHandler) {
+    function inspectionController($scope, Translate, ErrorHandler,EnvironmentConfig,URLS) {
         var vm = this;
         vm.asset = undefined;//objeto contenedor del cabinet
         vm.asset_id=''; //asset identifier
         vm.title_info=Translate.translate('INSPECTION.CHECKLIST');
         vm.assets_info=Translate.translate('INSPECTION.MORE_INFO');
         vm.checklist={};
+
+        const stickersURL =  (EnvironmentConfig.site.rest.api)
+            .concat('/' + URLS.entries_departures.base+ '/' + URLS.entries_departures.catalogues.base + '/' + URLS.entries_departures.catalogues.sticker);
+        vm.catalogues = {
+            sticker: {
+                catalog: {
+                    url:stickersURL,
+                    name: Translate.translate('INSPECTION.CATALOGUES.STICKER'),
+                    loadMoreButtonText: 'Cargar mas',
+                    model: 'id',
+                    option: 'descripcion'
+                },
+                pagination: {
+                    total: 'count',
+                    next: 'next'
+                },
+                required: true,
+                elements: 'results',
+                softDelete: {
+                    hide: 'deleted',
+                    reverse: false
+                },
+                noResults: Translate.translate('ERRORS.NO_RESULTS'),
+                hint:Translate.translate('SYMTOMPS_COMPONENT.ADD')
+            }
+        };
 
         //Declaración de Funciones como variable  de Componentes________________________________________________________
         vm.infogral=infogral;
--- src/app/mainApp/technical_service/inspection/inspection.provider.js
+++ src/app/mainApp/technical_service/inspection/inspection.provider.js
@@ -1,3 +1,42 @@
 /**
  * Created by franciscojaviercerdamartinez on 2/12/19.
  */
+(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .factory('inspectionProvider', inspectionProvider);
+
+    /* @ngInject */
+    function inspectionProvider($q,
+                                       URLS,
+                                       API) {
+
+        var urlbase = API.all(URLS.entries_departures.base)
+            .all(URLS.management.inventory.base)
+            .all(URLS.management.inventory.cabinet);
+
+        return {
+            makeInspection: makeInspection,
+            makePrecheck: makePrecheck,
+            makeChecklist:makeChecklist
+        };
+
+
+        function makeInspection(body,id) {
+            return urlbase.all(id).customPATCH();
+        }
+
+        function makePrecheck(body) {
+            return technicalServiceBase.all(URLS.technical_service.services.service).all(id_servicio).customPOST();
+        }
+
+        function makeChecklist(body) {
+            return technicalServiceBase.all(URLS.technical_service.services.service).all('?cabinet_economico='+economico).customPOST();
+        }
+
+
+
+    }
+})();
