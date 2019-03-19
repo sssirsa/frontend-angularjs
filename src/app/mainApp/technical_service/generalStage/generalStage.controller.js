799482eb4b97e0439b2b28cfdd79c41da5bcc555
franciscojaviercerdamartinez <fco.cerda.mtz@gmail.com>
2019-03-19 16:27:01 -0600

    Manejo de la validación de confirmación de guardado y cerrado

--- src/app/mainApp/technical_service/generalStage/generalStage.controller.js
+++ src/app/mainApp/technical_service/generalStage/generalStage.controller.js
@@ -9,18 +9,18 @@
         .module('app.mainApp.service')
         .controller('generalStageController', generalStageController);
 
-    function generalStageController($scope, Translate, toastr, ErrorHandler,diagnosisProvider) {
+    function generalStageController($scope, Translate, toastr, ErrorHandler, stageProvider, $mdDialog) {
         var vm = this;
 
         vm.asset = undefined;//objeto contenedor del cabinet
-        vm.asset_id=''; //asset identifier
-        vm.title_info=Translate.translate('GENERAL_STAGE.BULK_ASSET');
-        vm.assets_info=Translate.translate('GENERAL_STAGE.ACTIONS_MADE');
+        vm.asset_id = ''; //asset identifier
+        vm.title_info = Translate.translate('GENERAL_STAGE.BULK_ASSET');
+        vm.assets_info = Translate.translate('GENERAL_STAGE.ACTIONS_MADE');
 
         vm.stage = {
-            sucursal_id:undefined
+            sucursal_id: undefined
         };
-        vm.search=true;
+        vm.search = true;
 
         //Declaración de Funciones como variable  de Componentes________________________________________________________
         vm.infogral = infogral;
@@ -32,59 +32,58 @@
 
         //Declaracion de Funciones como variables_______________________________________________________________________
         vm.clear = clear;
-        vm.sendStage=sendStage;
-        vm.enableSearch=enableSearch;
+        vm.saveStage = saveStage;
+        vm.closeStage = closeStage;
+        vm.enableSearch = enableSearch;
 
         //Funciones Propias de la Pantalla
         function clear() {
-            vm.diagnostic = {
-                nombre_corto: undefined,
-                descripcion: undefined,
-                fallas_id: [],
-                en_tiempo: true,
-                temp_com: undefined,
-                temp_int: undefined,
-                amp_arran: undefined,
-                amp_trab: undefined,
+            vm.stage = {
                 etapa_siguiente_id: undefined,
-                acciones_id:[],
-                insumos_lote:[],
-                sucursal_id:undefined
+                acciones_id: [],
+                insumos_lote: [],
+                sucursal_id: undefined
             };
             vm.asset = undefined;
             vm.step = undefined;
         }
 
         function enableSearch() {
-            vm.search=!vm.search;
+            vm.search = !vm.search;
             clear();
 
         }
 
-
-
-        //  Funciones para Componentes _________________________________________________________________________________
-
-        function infogral(cabinet) {
-            vm.asset = cabinet;
-            vm.diagnostic.cabinet_id = vm.asset.economico;
-
-        }
-
-        function infoStep(step) {
-            console.log('etapa actual:');
-            console.log(step);
-            console.log(step.currentStage.id);
-            console.log(step.currentStage.servicio_cabinet);
-            vm.step = step;
-            vm.search=false;
-
-        }
-
-        function sendStage() {
+        function closeStage() {
             console.log(vm.stage)
-            var promiseSendDiagnosis=diagnosisProvider.sendDiagnosis(vm.step.currentStage.id,vm.diagnostic);
-            promiseSendDiagnosis.then(function(response){
+            vm.stage.sucursal_id = vm.step.control.sucursal.id;
+            var confirm;
+            confirm = $mdDialog.confirm()
+                .title(vm.dialogTitle)
+                .textContent(vm.confirmUpdate)
+                .ok(vm.accept)
+                .cancel(vm.cancel);
+            $mdDialog.show(confirm).then(function () {
+                var promiseSendDiagnosis = stageProvider.sendStage(vm.step.currentStage.id, vm.stage);
+                promiseSendDiagnosis.then(function (response) {
+                    console.info(response);
+                    clear();
+
+                }).catch(function (errormsg) {
+                    console.error(errormsg);
+                    ErrorHandler.errorTranslate(errormsg);
+                });
+            }, function () {
+            });
+        }
+        function saveStage() {
+            console.log(vm.stage)
+            if (vm.stage.etapa_siguiente_id) {
+                vm.stage = _.omit(vm.stage, etapa_siguiente_id);
+            }
+            vm.stage.sucursal_id = vm.step.control.sucursal.id;
+            var promiseSendDiagnosis = stageProvider.sendStage(vm.step.currentStage.id, vm.stage);
+            promiseSendDiagnosis.then(function (response) {
                 console.info(response);
                 clear();
 
@@ -94,6 +93,27 @@
             });
 
         }
+
+
+        //  Funciones para Componentes _________________________________________________________________________________
+
+        function infogral(cabinet) {
+            vm.asset = cabinet;
+            vm.stage.cabinet_id = vm.asset.economico;
+
+        }
+
+        function infoStep(step) {
+            console.log('etapa actual:');
+            console.log(step);
+            console.log(step.currentStage.id);
+            console.log(step.currentStage.servicio_cabinet);
+            vm.step = step;
+            vm.search = false;
+
+        }
+
+
 
         function getInsumosLote(element) {
             vm.checklist.insumos_lote = element;
@@ -110,17 +130,17 @@
             console.log('acciones detectadas:');
             console.log(element);
             vm.actions = element;
-            vm.diagnostic.acciones_id = [];
+            vm.stage.acciones_id = [];
             if (vm.actions) {
                 if (vm.actions.length > 0) {
 
                     var index;
                     for (index = 0; index < vm.actions.length; ++index) {
-                        vm.diagnostic.acciones_id.push(vm.actions[index].com_code);
+                        vm.stage.acciones_id.push(vm.actions[index].com_code);
                     }
                     console.log("Acciones");
                     console.log(vm.actions);
-                    console.log(vm.diagnostic.acciones_id);
+                    console.log(vm.stage.acciones_id);
                 }
             }
 
