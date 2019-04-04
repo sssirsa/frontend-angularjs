d3081614a042801cd87956ad207e3724e74837f7
franciscojaviercerdamartinez <fco.cerda.mtz@gmail.com>
2019-04-04 15:15:32 -0600

    retire unique asset tmpl y parte del controlador así como translates necesarios

--- src/app/mainApp/technical_service/components/retireUniqueAsset/retireUniqueAsset.controller.js
+++ src/app/mainApp/technical_service/components/retireUniqueAsset/retireUniqueAsset.controller.js
@@ -1,12 +1,7 @@
 /**
  * Created by franciscojaviercerdamartinez on 4/4/19.
  */
-/**
- * Created by franciscojaviercerdamartinez on 3/20/19.
- */
-/**
- * Created by franciscojaviercerdamartinez on 2/5/19.
- */
+
 
 (function () {
 
