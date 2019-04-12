d40a795142c9fe529691f8221b134ab17e6e0abd
ZedElric@Inspiron15/5000 <platae@gmail.com>
2019-04-12 15:16:07 -0500

    Defiición del constant de salepoint

---
+++ src/app/mainApp/salepoint/salepoint.constants.js
@@ -0,0 +1,13 @@
+(function () {
+    angular
+        .module('app.mainApp.salepoint')
+        .constant('SALEPOINT', {
+            base: 'salepoint',
+            catalogues: {
+                base: 'catalogues',
+                cities: 'municipio',
+                localities: 'localidad',
+                states:'estado'
+            }
+        });
+}) ();
