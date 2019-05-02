7a4503c1ec3523a4853f01ef15a0e45b95789484
ZedElric@Inspiron15/5000 <platae@gmail.com>
2019-05-01 21:25:19 -0500

    Corrección de LINT

--- src/app/mainApp/management/catalogues/bulk_asset/bulk_asset.controller.js
+++ src/app/mainApp/management/catalogues/bulk_asset/bulk_asset.controller.js
@@ -96,7 +96,7 @@
                                 reverse: false
                             }
                         },
-                        required: true,
+                        required: true
                     },
                     {
                         type: 'catalog',
