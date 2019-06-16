68fd8c2e6babfd1c437f3e85fc5fd48703edd0fd
franciscojaviercerdamartinez <fco.cerda.mtz@gmail.com>
2019-06-16 10:53:11 -0500

    Comparación de la tabla de control y la tabla de inventario utilizando un usuario con sucursal distinta a la del cabinet

--- src/app/mainApp/technical_service/components/searchCabinetStep/searchCabinetStep.controller.js
+++ src/app/mainApp/technical_service/components/searchCabinetStep/searchCabinetStep.controller.js
@@ -14,78 +14,88 @@
 
             }
         });
-    function searchCabinetStepController( Translate, searchCabinetStepProvider, ErrorHandler) {
+    function searchCabinetStepController(Translate, searchCabinetStepProvider, ErrorHandler, toastr) {
         var vm = this;
         vm.asset = {};//objeto contenedor del cabinet
         vm.asset_id = ''; //asset identifier
         vm.title_info = Translate.translate('INSPECTION.GENERAL_INFO');
         vm.assets_info = Translate.translate('INSPECTION.BULK_ASSETS');
+        vm.title = Translate.translate('SEARCH_INFO_CABINET_COMPONENT.NOT_IN_THIS_SUBSIDIARY_TITLE');
+        vm.messsage = Translate.translate('SEARCH_INFO_CABINET_COMPONENT.NOT_IN_THIS_SUBSIDIARY');
         vm.infoStep = {
-            makeInspection:undefined,
-            makePrecheck:undefined,
-            currentStage:undefined,
-            control:undefined
+            makeInspection: undefined,
+            makePrecheck: undefined,
+            currentStage: undefined,
+            control: undefined
         };
 
         vm.search_asset = search_asset;
-        function clear(){
+        function clear() {
             vm.infoStep = {
-                makeInspection:undefined,
-                makePrecheck:undefined,
-                currentStage:undefined,
-                control:undefined
+                makeInspection: undefined,
+                makePrecheck: undefined,
+                currentStage: undefined,
+                control: undefined
             };
             vm.asset = {};//objeto contenedor del cabinet
         }
+
         function search_asset() {
             //Limpiamos las variables antes de hacer una nueva búsqueda
             clear();
             //Search in cabinets location
-            var promiseCabinetInfo = searchCabinetStepProvider.getByID(vm.asset_id);
-            promiseCabinetInfo.then(function (asset) {
-                vm.asset = asset;
-                //selection subsidiary
-                //console.log(vm.asset);
-                vm.infoGral({element: vm.asset});
-                var promiseCabinetEntrada = searchCabinetStepProvider.getEntrie(vm.asset.economico);
-                promiseCabinetEntrada.then(function (control) {
+            var promiseCabinetEntrada = searchCabinetStepProvider.getEntrie(vm.asset_id);
+            promiseCabinetEntrada.then(function (control) {
+                vm.infoStep.control = control;
+                if (control.tipo_entrada === "Garantias" || control.tipo_entrada === "Reparacion") {
+                    vm.infoStep.makeInspection = false;
+                } else {
+                    vm.infoStep.makePrecheck = false;
+                    vm.infoStep.makeInspection = true;
+                    vm.infoStepToDo({element: vm.infoStep});
 
-                    vm.infoStep.control=control;
-                    if (control.tipo_entrada === "Garantias"||control.tipo_entrada==="Reparacion") {
-                        vm.infoStep.makeInspection = false;
-                        var promiseGetCurrentStage = searchCabinetStepProvider.getCurrentStage(vm.asset.economico);
-                        promiseGetCurrentStage.then(function (currentStage) {
-                            vm.infoStep.currentStage = currentStage;
-                            vm.infoStep.makePrecheck = false;
-                            vm.infoStepToDo({element: vm.infoStep});
-                        }).catch(function (errormsg) {
+                }
 
-                            if (errormsg.status == 404) {
-                                vm.infoStep.makePrecheck = true;
-                                vm.infoStep.makeInspection  = false;
-                                vm.infoStepToDo({element: vm.infoStep});
+                var promiseCabinetInfo = searchCabinetStepProvider.getByID(vm.asset_id);
+                promiseCabinetInfo.then(function (asset) {
+                    vm.asset = asset;
+                    //selection subsidiary
+                    //console.log(vm.asset);
+                    vm.infoGral({element: vm.asset});
 
-                            }
-                            else {
-                                ErrorHandler.errorTranslate(errormsg);
-                            }
-                        });
-                    } else {
-                        vm.infoStep.makePrecheck  = false;
-                        vm.infoStep.makeInspection  = true;
-
+                    var promiseGetCurrentStage = searchCabinetStepProvider.getCurrentStage(vm.asset_id);
+                    promiseGetCurrentStage.then(function (currentStage) {
+                        vm.infoStep.currentStage = currentStage;
+                        vm.infoStep.makePrecheck = false;
                         vm.infoStepToDo({element: vm.infoStep});
+                    }).catch(function (errormsg) {
+                        if (errormsg.status === 404) {
+                            vm.infoStep.makePrecheck = true;
+                            vm.infoStep.makeInspection = false;
+                            vm.infoStepToDo({element: vm.infoStep});
 
+                        }
+                        else {
+                            ErrorHandler.errorTranslate(errormsg);
+                        }
+                    });
+                }).catch(function (errormsg) {
+                    if (angular.isDefined(vm.infoStep.control.sucursal) || vm.infoStep.control.udn) {
+                        toastr.error(vm.title, vm.messsage);
+                        clear();
+                    }
+                    else {
+                        ErrorHandler.errorTranslate(errormsg);
+                        clear();
                     }
 
-                }).catch(function (errormsg) {
-                    ErrorHandler.errorTranslate(errormsg);
                 });
 
 
             }).catch(function (errormsg) {
                 ErrorHandler.errorTranslate(errormsg);
             });
+
 
         }
 
