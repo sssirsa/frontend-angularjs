ec6d1ed9992beb1cd8530745580e367f31b91099
franciscojaviercerdamartinez <fco.cerda.mtz@gmail.com>
2019-07-16 18:43:16 -0500

    creacion de modulo para cargas masivas

---
+++ src/app/mainApp/massiveCharge/historical/historical.controller.js
@@ -0,0 +1,38 @@
+/**
+ * Created by franciscojaviercerdamartinez on 7/16/19.
+ */
+
+(function () {
+    'use strict';
+
+    angular
+        .module('app.mainApp.massiveCharge')
+        .controller('HistoricalController', historicalController);
+
+    function historicalController($scope, Translate, toastr, ErrorHandler, $log) {
+        var vm = this;
+        vm.variable="";
+        $log.log(vm.variable);
+
+        //Declaración de Funciones como variable  de Componentes________________________________________________________
+
+
+
+        //--------------------------------------------------------------------------------------------------------------
+
+        //Declaracion de Funciones como variables_______________________________________________________________________
+
+
+        //Funciones Propias de la Pantalla
+
+
+
+        //  Funciones para Componentes _________________________________________________________________________________
+
+        //--------------------------------------------------------------------------------------------------------------
+
+
+    }
+
+
+})();
---
+++ src/app/mainApp/massiveCharge/massiveCharge.constants.js
@@ -0,0 +1,10 @@
+/**
+ * Created by franciscojaviercerdamartinez on 7/16/19.
+ */
+(function () {
+    angular
+        .module('app.mainApp.massiveCharge')
+        .constant('MASSIVE_CHARGE', {
+
+        });
+})();
