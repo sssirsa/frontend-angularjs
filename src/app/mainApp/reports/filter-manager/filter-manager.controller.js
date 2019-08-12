9cf87aeb538c219b18b32d615ad934770b4eeda3
Platae <platae93@gmail.com>
2019-08-11 22:00:10 -0500

    Creación de arcivos base para componente de filtros

---
+++ src/app/mainApp/reports/filter-manager/filter-manager.controller.js
@@ -0,0 +1,61 @@
+/*
+ * Filters object
+ *  filters:{
+ *      query:value
+ *  }
+ *  Example:
+ *  filters:{
+ *      field__property__filter_type:{{filterValue}}
+ *  }
+ */
+
+(function () {
+    angular
+        .module('app.mainApp.reports')
+        .component('filterManager', {
+            templateUrl: 'app/mainApp/reports/filter-manager/filter-manager.tmpl.html',
+            controller: filterManagerController,
+            controllerAs: 'vm',
+            bindings: {
+                //Main Parameters
+                filters: '<',      //The filter array returned by the API
+                queries: '=',      //Bidirectional binding for the query parameters object, should be passed as an empty array
+                //Button Labels
+                addButton: '<',    //Default is "Add"
+                removeButton: '<', //Default is "Remove"
+                modifyButton: '<', //Default is "Modify"
+                //Field labels
+                fieldLabel: '<',    //Default is "field"
+                propertyLabel: '<', //Default is "property"
+                filtersLabel: '<',  //Default is "Filter to apply"
+                valueLabel: '<',     //Default is "value"
+            }
+        });
+
+    function filterManagerController(
+        $log
+    ) {
+        var vm = this;
+
+        //Globals
+        vm.appliedFilters;
+        /*
+         * {
+         *      query: string,
+         *      verbose: string
+         * }
+         */
+
+        var init = function () {
+            vm.appliedFilters = [
+                {
+                    query: "this is the query",
+                    verbose: "a verbose for the filter"
+                }
+            ];
+        };
+        init();
+
+
+    }
+})();
