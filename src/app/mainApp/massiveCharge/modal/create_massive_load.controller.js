32f49119b82c28a046146a377c8d30dca3721c0b
franciscojaviercerdamartinez <fco.cerda.mtz@gmail.com>
2019-08-13 01:56:44 -0500

    Listo la creación de cargas masivas

--- src/app/mainApp/massiveCharge/modal/create_massive_load.controller.js
+++ src/app/mainApp/massiveCharge/modal/create_massive_load.controller.js
@@ -7,16 +7,17 @@
         .module('app.mainApp')
         .controller('createMassiveLoadController', createMassiveLoadController);
 
-    function createMassiveLoadController($mdDialog,  ErrorHandler, MassiveLoadProvider) {
+    function createMassiveLoadController($mdDialog,ErrorHandler, MassiveLoadProvider,Upload, EnvironmentConfig,$cookies,toastr,$log,Translate,MASSIVE_CHARGE) {
         var vm = this;
 
         //variables
-        vm.infoMassiveLoad;
+
 
         //funciones
         vm.cerrar = cerrar;
         vm.clean = clean;
         vm.selectFiles=selectFiles;
+        vm.save=save;
 
         init();
 
@@ -44,6 +45,28 @@
             vm.tipo=null;
             vm.files=null;
         }
+        function save() {
+            if (vm.files.length > 0) {
+                var file ={
+                    bulk_load_id:vm.tipo.id,
+                    file:vm.files[0]};
+                vm.uploadPromise = Upload.upload({
+                    url: EnvironmentConfig.site.rest.api + '/' + MASSIVE_CHARGE.project+'/'+ MASSIVE_CHARGE.base+'/'+MASSIVE_CHARGE.actions.bulk,
+                    headers: {'Authorization': "Bearer " + $cookies.get('token')},
+                    method: 'POST',
+                    data: file
+                })
+                    .then(function () {
+                        toastr.success(Translate.translate('MAIN.COMPONENTS.STORE_MANAGER.TOASTR.MASSIVE_SUCCESS'));
+                        cerrar();
+                    })
+                    .catch(function (errorUpload) {
+                        $log.error(errorUpload);
+                        toastr.error(Translate.translate('MAIN.COMPONENTS.STORE_MANAGER.TOASTR.MASSIVE_ERROR'));
+                    });
+            }
+        }
+
 
     }
 
